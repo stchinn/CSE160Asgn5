5932e9854297dcff91d4f7590e8bf778b62be022
Spencer Chinn <stchinn@ucsc.edu>
2025-03-12 17:38:45 -0700

    rebuilding

--- src/world.js
+++ src/world.js
@@ -2,10 +2,10 @@
 // Used various guides online, threejs.org guides and code as a base, and chatgpt to assist
 
 import * as THREE from 'three';
-import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
-import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // GLOBALS
 let scene = new THREE.Scene();
